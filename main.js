6a3d740391cec1ef770e4500029d385c54e5c65b
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-11-09 14:49:26 -0700

    fd

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
-let results []
+// let results = [
+//   "yes",
+//   "no",
+//   "maybe",
+//   "yes",
+//   "yes",
+//   "no",
+//   "maybe",
+//   "yes",
+//   "yes",
+//   "yes",
+//   "no",
+//   "maybe",
+//   "yes",
+//   "yes",
+//   "no",
+//   "maybe",
+// ];
+
+// let yesCount = 0;
+// let noCount = 0;
+// let maybeCount = 0;
+// for (let x = 0; x < results.length; x++) {
+//   if (results[x] == "yes") {
+//     yesCount++;
+//   } else if (results[x] == "no") {
+//     noCount++;
+//   } else {
+//     maybeCount++;
+//   }
+// }
+
+// console.log(yesCount, noCount, maybeCount);
+
+ let results = [
+    "yes",
+    "no",
+    "maybe",
+    "yes",
+    "yes",
+    "no",
+    "maybe",
+    "yes",
+    "yes",
+    "yes",
+    "no",
+    "maybe",
+    "yes",
+    "yes",
+    "no",
+    "maybe",
+  ];
+  let yescount = 0;
+  let nocount = 0;
+  let maybecount = 0; 
+  for(let x=0; x<results.length; x++) {
+      if (results[x] ==="yes") {
+          yescount++
+      } else if(results[x] ==="no") {
+          nocount++
+      } else {
+          maybecount++
+      }
+  }
+  
+  console.log(yescount, nocount, maybecount);
+  
+  
+
+
+
+
